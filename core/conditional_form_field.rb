0620433e7e1e0d9043979e99d7a3776215c3742c
Joris van Rooij <jorrizza@jrrzz.net>
2010-11-09 21:29:51 +0100

    Added object source to conditional form field.

--- core/conditional_form_field.rb
+++ core/conditional_form_field.rb
@@ -1,9 +1,12 @@
-# Simple helper to display form field data when available
+# Simple helper to display form field data when available.
+# It prefers params, but when unavailable it'll use the supplied object (if available).
 
 helpers do
-  def c(field)
+  def c(field, object = nil)
     if params.has_key? field.to_s
       params[field.to_s].html
+    elsif object.respond_to? field
+      object.send(field).html
     else
       ""
     end
