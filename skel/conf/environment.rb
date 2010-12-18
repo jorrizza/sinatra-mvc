f2897b32b33b026e3236246037234365a7793d52
Joris van Rooij <jorrizza@jrrzz.net>
2010-12-18 19:52:45 +0100

    Forgot to add env file.

---
+++ skel/conf/environment.rb
@@ -0,0 +1,9 @@
+# Environment File
+# Require all the libraries used in your project here.
+
+# Datamapper Modules
+require 'dm-aggregates'
+
+# Templating Engines
+#require 'redcloth'
+#require 'liquid'
